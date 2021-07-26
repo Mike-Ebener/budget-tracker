6f41f869e84e560b7155cec2e8defe67015275be
ME <mebener44@gmail.com>
2021-07-25 22:00:41 -0700

    working indexedDB functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -16,10 +16,10 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    // check if app is online, if yes run uploadBudget() function to send all local db data to api
     if (navigator.onLine) {
       // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadPizza();
+      uploadBudget();
     }
   };
   
@@ -50,5 +50,40 @@ function saveRecord(record) {
     // get all records from store and set to a variable
     const getAll = budgetObjectStore.getAll();
   
-    // more to come...
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+        //(NEW) api/transaction vs. api/pizza
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          // access the new_pizza object store
+          const budgetObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All saved transactions have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+
   }
+
+  // listen for app coming back online
+window.addEventListener('online', uploadBudget);
